5653a51a5bf778495aea494a48333f00677a9006
kimhj <guswlrla1999@naver.com>
2020-04-22 22:22:14 +0900

    added button, editText ~~ MainActivity.xml

--- app/src/main/java/com/example/ansys/hahahaha/MainActivity.java
+++ app/src/main/java/com/example/ansys/hahahaha/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.ansys.hahahaha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit_name;
+    Button start_btn, about_btn;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_name = findViewById(R.id.edit_name);
+        start_btn = findViewById(R.id.start_btn);
+        about_btn = findViewById(R.id.about_btn);
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str = edit_name.getText().toString();
+
+            }
+        });
+
     }
 }
